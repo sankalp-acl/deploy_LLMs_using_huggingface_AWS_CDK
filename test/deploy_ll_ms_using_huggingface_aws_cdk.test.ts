013c86558d7cee9a707cfa0ebcd7f0dcb971c431
Sankalp Prabhakar <sprabhakar@diligent.com>
2023-08-29 17:12:50 -0600

    Initial commit

---
+++ test/deploy_ll_ms_using_huggingface_aws_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DeployLlMsUsingHuggingfaceAwsCdk from '../lib/deploy_ll_ms_using_huggingface_aws_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/deploy_ll_ms_using_huggingface_aws_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DeployLlMsUsingHuggingfaceAwsCdk.DeployLlMsUsingHuggingfaceAwsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
